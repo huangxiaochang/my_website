73ccbb4033cda4afadbb6fc355ec621d69e1cba0
huangxiaochang <wb.huangxiaochang@mesg.corp.netease.com>
2018-05-28 19:20:36 +0800

    增加获取验证码

--- src/api/user_info.js
+++ src/api/user_info.js
@@ -3,3 +3,7 @@ import axios from './index.js'
 export function userRegister (data) {
 	return axios.post(`/register/`, data).then((response) => response)
 }
+
+export function getCode (email) {
+	return axios.get(`/register/?email=${email}`).then((response) => response)
+}
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -5,15 +5,15 @@
 			<label class="form_item"><span>用户名:</span><input v-model="form.account_name" class="input_item" type="text" placeholder="请输入用户名"></label>
 			<label class="form_item"><span>密码:</span><input v-model="form.password" class="input_item" type="password" placeholder="请输入密码"></label>
 			<label class="form_item"><span>确认密码:</span><input v-model="form.password2" class="input_item" type="password" placeholder="请输入密码"></label>
-			<label class="form_item"><span>邮箱:</span><input v-model="form.user_emit" class="input_item" type="text" placeholder="请输入邮箱"></label>
-			<label class="form_item"><span>验证码:</span><input v-model="form.verification_code" class="input_item" type="text" placeholder="请输入验证码"><button class="btn_default">获取验证码</button></label>
+			<label class="form_item"><span>邮箱:</span><input v-model="form.user_email" class="input_item" type="text" placeholder="请输入邮箱"></label>
+			<label class="form_item"><span>验证码:</span><input v-model="form.verification_code" class="input_item" type="text" placeholder="请输入验证码"><button class="btn_default" @click="get_code">获取验证码</button></label>
 			<button class="btn_primary" @click="save">确定</button>
 		</form>
 	</section>
 </template>
 
 <script type="text/babel">
-	import { userRegister } from 'api/user_info.js'
+	import { userRegister, getCode } from 'api/user_info.js'
 
 	export default {
 		name: 'login',
@@ -23,7 +23,7 @@
 					account_name: '',
 					password2: '',
 					password: '',
-					user_emit: '',
+					user_email: '',
 					verification_code: ''
 				}
 			}
@@ -32,6 +32,11 @@
 			save () {
 				userRegister(this.form).then((res) => {
 					console.log(res)
+				})
+			},
+			get_code () {
+				getCode(this.form.user_email).then((res) => {
+
 				})
 			}
 		}
